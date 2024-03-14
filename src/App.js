dee514067cb27ba7974e2db6d7ee4a385413d62d
gfjfdh <devchauhanchauhan746@gmail.com>
2024-03-14 03:33:18 -0700

    react work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { useState } from "react";
+import moon from './component/image/moon.jpg'
 
 function App() {
+
+  let [count,setCount] = useState(1)
+  let [visible,setvisible] = useState(true)
+  console.log(count)
+
+
+  function increaseCounter(){
+    count++
+    setCount(count)
+  }
+
+  function decreaseCounter(){
+    count--
+    setCount(count)
+  }
+
+
+  function show(){
+    setvisible(true)
+  }
+
+  function hide(){
+    setvisible(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <h1>{count}</h1>
+           <button onClick = {increaseCounter}>increase</button>
+           <button onClick = {decreaseCounter}>Decrease</button> <br /> <br />
+
+
+           {(visible==true)?<img src ={moon} width={350}/>:<h1>image is hide now</h1>} <br />
+           <button onClick={show}>show</button>
+           <button onClick={hide}>Hide</button>
+           
+
     </div>
   );
 }
